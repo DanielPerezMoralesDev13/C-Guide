506f3e638280cb2f677feb178fee74387bc95e23
d4nitrix13 <danielperezdev@proton.me>
2024-07-13 14:50:47 -0600

    update

--- lenguage/59 Stdin Stdout.c
+++ lenguage/59 Stdin Stdout.c
@@ -3,16 +3,17 @@
 // Correo electrónico: danielperezdev@proton.me
 #include <stdio.h>
 
-int main(void) {
-    // Redirigiendo la salida estándar a un archivo
-    FILE *fichero = stdout;  // Redirige a la salida estándar
+int main(void)
+{
+    // Redirigiendo la salida estándar a un fichero
+    FILE *fichero = stdout; // Redirige a la salida estándar
     fprintf(fichero, "Este es un mensaje que será redirigido a la salida estándar (stdout).\n");
 
     // Leyendo desde la entrada estándar
     char buffer[100];
     printf("Por favor, ingrese una cadena: ");
-    fgets(buffer, sizeof(buffer), stdin);  // Lee desde la entrada estándar
-    fprintf(stdout, "Usted ingresó: %s", buffer);  // Escribe en la salida estándar
+    fgets(buffer, sizeof(buffer), stdin);         // Lee desde la entrada estándar
+    fprintf(stdout, "Usted ingresó: %s", buffer); // Escribe en la salida estándar
 
     return 0;
 }
