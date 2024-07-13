ad680cf200cb1bc805e0659cec199fcb31bb6849
d4nitrix13 <danielperezdev@proton.me>
2024-07-13 14:51:03 -0600

    update

---
+++ dev/program list dinamic type int/src/main.c
@@ -0,0 +1,24 @@
+// Autor: Daniel Benjamin Perez Morales
+// GitHub: https://github.com/DanielPerezMoralesDev13
+// Correo electrónico: danielperezdev@proton.me
+#include "../lib/lib.h"
+
+#define CODE_EXITOSO return 0
+#define CODE_NO_EXITOSO return 1
+
+int main(void){
+    List *lista = crearListaInt();
+    
+    if (lista == NULL){
+        perror("List *lista = crearListaInt(); -> Error al asignar la memoria");
+        CODE_NO_EXITOSO;
+    }
+    for (int i = 0; i < 10; i++) {
+        añadirItem(lista,i);   
+    }
+    imprimirListaInt(lista);
+    eliminarItemValor(lista,2);
+    imprimirListaInt(lista);
+    liberarListInt(lista);
+    CODE_EXITOSO;
+}
